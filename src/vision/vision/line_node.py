9d4e64625405de6fad308cc4200101df1812225b
TheoC04 <theocowen2004@gmail.com>
2026-04-29 19:43:12 +0100

    reduced que size to reduce lag

--- src/vision/vision/line_node.py
+++ src/vision/vision/line_node.py
@@ -479,7 +479,7 @@ class LineDetectionNode(Node):
             CompressedImage,
             '/camera/image_raw/compressed',
             self.image_callback,
-            10
+            1
         )
 
         self.image_publisher = self.create_publisher(
